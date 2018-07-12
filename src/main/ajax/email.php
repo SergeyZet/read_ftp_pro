b21c6c5a2324ad9621d9c49de9351b9e4e6f64df
Иван Падерин <paderin.vanya2017@yandex.ru>
2018-07-12 15:16:41 +0600

    Добавил свой backend для сохранения обращений(email) в базе данных

--- src/main/ajax/email.php
+++ src/main/ajax/email.php
@@ -2,6 +2,6 @@
 require_once ('../email/Email.php');
 $json = json_decode($_POST['json']);
 $email = new Email($json->name,$json->phone,$json->email,$json->coment);
-//$email->send();
+$email->send();
 $email->send_database();
 print_r($json);
